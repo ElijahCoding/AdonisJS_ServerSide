8bb71c2a1fa449ef122a8b2de1b4b9b4fbc7adcd
Elijah <elijahsharper1@gmail.com>
2018-03-27 15:25:42 +0300

    reply validation

--- app/Controllers/Http/PostReplyController.js
+++ app/Controllers/Http/PostReplyController.js
@@ -1,8 +1,28 @@
 'use strict'
 
+const { validateAll } = use('Validator')
+const Post = use('App/Models/Post')
+
 class PostReplyController {
-  store ({ request, response }) {
-    return 'works'
+  async store ({ request, response, session, auth, params }) {
+    let post = await Post.query()
+        .where('slug', '=', params.slug)
+        .firstOrFail()
+
+    const { body } = request.all()
+
+    const rules = {
+      body: 'required'
+    }
+
+    const validation = await validateAll(request.all(), rules)
+
+    if (validation.fails()) {
+      session.withErrors(validation.messages())
+             .flashAll()
+
+      return response.redirect('back')
+    }
   }
 }
 
