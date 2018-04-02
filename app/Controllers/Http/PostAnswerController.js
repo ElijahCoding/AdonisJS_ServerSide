34fd9b2df388167ed493ed46aac3c3de6aa17ae4
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-02 14:03:06 +0300

    best answer controller validation

--- app/Controllers/Http/PostAnswerController.js
+++ app/Controllers/Http/PostAnswerController.js
@@ -1,8 +1,33 @@
 'use strict'
 
+const validateAll = use('Validator')
+
 class PostAnswerController {
-  store () {
-    return 'works'
+  async store ({ request, response, auth, params }) {
+    const answer_id = request.all()
+
+    let post = await Post.query()
+        .where('slug', '=', params.slug)
+        .where('user_id', '=', auth.user.id)
+        .whereHas('replies', (builder) => {
+          builder.where('id', '=', answer_id)
+        })
+        .firstOrFail()
+
+    const rules = {
+      answer_id: 'required|exists:posts,id'
+    }
+
+    const validation = await validateAll(request.all(), rules)
+
+    if (validation.fails()) {
+      session.withErrors(validation.messages())
+             .flashAll()
+
+      return response.redirect('back')
+    }
+
+    
   }
 }
 
